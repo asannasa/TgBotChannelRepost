4d4c1406743b5d70f1d283cf09a503e2469f38c9
Anton Asabin <asannasa@mail.ru>
2023-07-05 14:43:39 +0500

    Fixed problem with reposting single or group messages. Added logging.

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
+import logging
 import config as cfg
 from telethon import TelegramClient, events
 
-
+logging.basicConfig(filename='TgBot.log', encoding='utf-8', level=logging.INFO)
 client = TelegramClient('session_name', cfg.api_id, cfg.api_hash)
+
+
+@client.on(events.Album(chats=cfg.channel_id))
+async def handler_snd(event):
+    for chat_id_n in cfg.chats_id:
+        try:
+            await client.forward_messages(chat_id_n, event.messages)
+        except ValueError as err:
+            logging.warning(err)
 
 
 @client.on(events.NewMessage(chats=cfg.channel_id))
 async def normal_handler(event):
-    try:
-        with open("chat_id.txt", "r") as chat_f:
-            for chat_id_f in chat_f:
-                try:
-                    await client.forward_messages(int(chat_id_f), event.message)
-                except ValueError as err:
-                    print(err)
-    except IOError:
-        print('File no')
+    if not event.grouped_id:
+        for chat_id_n in cfg.chats_id:
+            try:
+                await client.forward_messages(chat_id_n, event.message)
+            except ValueError as err:
+                logging.warning(err)
 
 
 if __name__ == '__main__':
